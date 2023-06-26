0c218bed1072115922bda50862a9d2ff7f78c5b0
Aman Harsh <amanharsh0399@gmail.com>
2023-06-26 06:12:13 +0530

    feat: upload image and edit post image
    Implemented upload image feature while creating a post and editing the image of an existing post.

--- src/components/CreateNewPost/CreateNewPost.jsx
+++ src/components/CreateNewPost/CreateNewPost.jsx
@@ -5,32 +5,53 @@ import { useEffect, useRef, useState } from "react";
 
 export function CreateNewPost({ postId, edit = false }) {
   const { user, posts } = useData();
-  const { handleCreatePost, handleEditPost } = usePost();
+  const { handleCreatePost, handleEditPost, handleMediaUpload } = usePost();
+  const [selectedPost, setSelectedPost] = useState(false);
   const [errorState, setErrorState] = useState(false);
+  const [mediaUrl, setMediaUrl] = useState("");
   const postContentRef = useRef("");
-  const mediaUrl = "";
-  let selectedPost = null;
+  let postFound = null;
 
-  if (edit) {
-    selectedPost = posts.find(({ _id }) => _id === postId);
-  }
+  useEffect(() => {
+    if (edit) {
+      postFound = posts.find(({ _id }) => _id === postId);
+      setSelectedPost(postFound);
+    }
+  }, []);
+
+  useEffect(() => {
+    setMediaUrl(selectedPost?.mediaURL);
+  }, [selectedPost]);
 
   const handlePostClick = (e) => {
-    const editOrPost = e.target.value;
+    const editOrPost = e.target.innerText;
 
     if (editOrPost === "Post") {
       if (postContentRef?.current?.value?.length <= 0) {
         setErrorState(true);
       } else {
-        handleCreatePost(postContentRef?.current?.value);
+        handleCreatePost(postContentRef?.current?.value, mediaUrl);
         postContentRef.current.value = "";
+        setMediaUrl("");
       }
     } else {
       if (postContentRef?.current?.value?.length <= 0) {
         setErrorState(true);
       } else {
-        handleEditPost(selectedPost?._id, postContentRef?.current?.value);
+        handleEditPost(
+          selectedPost?._id,
+          postContentRef?.current?.value,
+          mediaUrl
+        );
       }
+    }
+  };
+
+  const handleMediaClick = async (e) => {
+    const response = await handleMediaUpload(e.target.files[0]);
+
+    if (response) {
+      setMediaUrl(response);
     }
   };
 
@@ -45,7 +66,7 @@ export function CreateNewPost({ postId, edit = false }) {
 
       <hr></hr>
 
-      <div className="flex flex-row gap-2 h-[130px] p-2">
+      <div className="flex flex-row gap-2 h-max p-2">
         <div className="h-[100%] w-max flex justify-center ">
           <img
             src={user.avatarUrl}
@@ -54,23 +75,40 @@ export function CreateNewPost({ postId, edit = false }) {
           />
         </div>
 
-        <textarea
-          ref={postContentRef}
-          defaultValue={edit ? selectedPost?.content : ""}
-          onChange={() => setErrorState(false)}
-          placeholder="Have something to share? Why not post it here!"
-          className="grow resize-none outline-none p-1 placeholder:font-light"
-        />
+        <div className="flex flex-col gap-4 grow">
+          <textarea
+            ref={postContentRef}
+            defaultValue={edit ? selectedPost?.content : ""}
+            onChange={() => setErrorState(false)}
+            placeholder="Have something to share? Why not post it here!"
+            className="resize-none outline-none p-1 placeholder:font-light min-h-[150px]"
+          />
+
+          {mediaUrl && <hr></hr>}
+
+          {mediaUrl && (
+            <img
+              src={mediaUrl}
+              alt="post"
+              className="w-max max-h-[150px] rounded-md"
+            />
+          )}
+        </div>
       </div>
 
       <hr></hr>
 
       <div className="flex items-center justify-between p-3">
         <div className="flex gap-2 items-center">
-          <button className="flex items-center gap-1 bg-blue-100 text-slate-400 px-4 py-1 rounded-2xl hover:bg-blue-200 hover:text-slate-800 hover:shadow-md">
+          <label className="flex items-center gap-1 cursor-pointer bg-blue-100 text-slate-400 px-4 py-1 rounded-2xl hover:bg-blue-200 hover:text-slate-800 hover:shadow-md">
+            <input
+              type="file"
+              onChange={(e) => handleMediaClick(e)}
+              className="hidden"
+            />
             <MediaIcon />
             Media
-          </button>
+          </label>
           <div className="cursor-pointer text-xl">😀</div>
         </div>
 
@@ -85,6 +123,12 @@ export function CreateNewPost({ postId, edit = false }) {
       {errorState && (
         <div className="flex px-4 pb-4 text-red-500">
           * Please write something to post!
+        </div>
+      )}
+
+      {mediaUrl && (
+        <div className="flex px-4 pb-4 text-green-500">
+          * Media uploaded successfully!
         </div>
       )}
     </div>
