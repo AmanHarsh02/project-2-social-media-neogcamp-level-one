0c7a1068bd38fea43be2ab83bee2a47354e700ed
Aman Harsh <amanharsh0399@gmail.com>
2023-06-28 04:05:24 +0530

    feat: started setting up the profile page
    started setting up the profile page

--- src/components/SideNavigation/SideNavigation.jsx
+++ src/components/SideNavigation/SideNavigation.jsx
@@ -65,7 +65,7 @@ export function SideNavigation({ setShowPostModal }) {
           <p className="hidden lg:block">Liked</p>
         </NavLink>
         <NavLink
-          to="/profile/:userName"
+          to={`/profile/${user.username}`}
           className={({ isActive }) => {
             const classes =
               "flex items-center gap-2 p-2 lg:pr-6 w-max rounded-full text-xl font-medium hover:bg-blue-100 hover:shadow-md";
