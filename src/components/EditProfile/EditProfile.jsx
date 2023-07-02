e1729e92400ada087fea757b6e990f8a4c39f785
Aman Harsh <amanharsh0399@gmail.com>
2023-07-02 21:26:50 +0530

    feat: implemented edit user profile feature

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -1,33 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   BiUserCircle as UserIcon,
   BiEditAlt as EditIcon,
   BiCamera as MediaIcon,
 } from "react-icons/bi";
 import { AvatarOptions } from "../AvatarOptions/AvatarOptions";
+import { useData } from "../../contexts/DataContext";
 
 export function EditProfile({ user, setShowEditProfile }) {
+  const { handleEditProfile, handleProfilePictureUpload } = useData();
+  const [imageUploadLoading, setImageUploadLoading] = useState(false);
+
+  const [newUserDetails, setnewUserDetails] = useState({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    username: user.username,
+    avatarUrl: user.avatarUrl,
+    bio: user.bio,
+    website: user.website,
+  });
+  const { firstName, lastName, username, bio, website } = newUserDetails;
+
   const [showImageOption, setShowImageOption] = useState(false);
   const [showAvatarOption, setShowAvatarOption] = useState(false);
   const [selectedImage, setSelectedImage] = useState(user?.avatarUrl);
-
-  const { firstName, lastName, username, avatarUrl, bio, website } = user ?? {
-    firstName: "",
-    lastName: "",
-    username: "",
-    avatarUrl: "",
-    bio: "",
-    website: "",
-  };
 
   const handleClick = (e) => {
     const buttonClicked = e.target.innerText;
 
     if (buttonClicked === "Save") {
+      handleEditProfile(newUserDetails);
+      setShowEditProfile(false);
     } else {
       setShowEditProfile(false);
     }
   };
+
+  const handleMediaClick = async (e) => {
+    setImageUploadLoading(true);
+    setShowImageOption(!showImageOption);
+    const response = await handleProfilePictureUpload(e.target.files[0]);
+
+    if (response) {
+      setSelectedImage(response);
+      setImageUploadLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setnewUserDetails({ ...newUserDetails, avatarUrl: selectedImage });
+  }, [selectedImage]);
 
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-slate-800 bg-opacity-50 z-20">
@@ -42,7 +64,7 @@ export function EditProfile({ user, setShowEditProfile }) {
             <img
               src={selectedImage}
               alt={`${username}'s profile`}
-              className="h-[100px] md:h-[210px] shrink-0 object-cover rounded-full shadow-md shadow-slate-600"
+              className="h-[100px] md:h-[210px] w-[100%] shrink-0 object-cover rounded-full shadow-md shadow-slate-600"
             />
             <div className="absolute bottom-4 right-4 md:bottom-6 md:right-6 cursor-pointer bg-slate-200 hover:bg-slate-300 w-max p-2 rounded-full shadow-md">
               <MediaIcon
@@ -54,10 +76,15 @@ export function EditProfile({ user, setShowEditProfile }) {
 
           {showImageOption && (
             <div className="absolute -bottom-[4.5rem] -right-[1.5rem] md:right-[1.5rem] w-max bg-slate-200 rounded-lg overflow-hidden">
-              <div className="flex gap-1 items-center cursor-pointer hover:bg-slate-300 p-2">
+              <label className="flex gap-1 items-center cursor-pointer hover:bg-slate-300 p-2">
+                <input
+                  type="file"
+                  onChange={(e) => handleMediaClick(e)}
+                  className="hidden"
+                />
                 <MediaIcon className="w-[18px] h-[18px] md:w-[24px] md:h-[24px]" />
-                <p>Upload profile picture</p>
-              </div>
+                Upload profile picture
+              </label>
 
               <hr className="border-gray-400"></hr>
 
@@ -82,6 +109,12 @@ export function EditProfile({ user, setShowEditProfile }) {
           <input
             type="text"
             defaultValue={firstName}
+            onChange={(e) =>
+              setnewUserDetails({
+                ...newUserDetails,
+                firstName: e.target.value,
+              })
+            }
             className="max-w-[100%] border border-slate-400 px-2 py-1 rounded-md"
           />
         </label>
@@ -91,6 +124,9 @@ export function EditProfile({ user, setShowEditProfile }) {
           <input
             type="text"
             defaultValue={lastName}
+            onChange={(e) =>
+              setnewUserDetails({ ...newUserDetails, lastName: e.target.value })
+            }
             className="max-w-[100%] border border-slate-400 px-2 py-1 rounded-md"
           />
         </label>
@@ -100,6 +136,9 @@ export function EditProfile({ user, setShowEditProfile }) {
           <input
             type="text"
             defaultValue={bio}
+            onChange={(e) =>
+              setnewUserDetails({ ...newUserDetails, bio: e.target.value })
+            }
             className="max-w-[100%] border border-slate-400 px-2 py-1 rounded-md"
           />
         </label>
@@ -109,12 +148,18 @@ export function EditProfile({ user, setShowEditProfile }) {
           <input
             type="text"
             defaultValue={website}
+            onChange={(e) =>
+              setnewUserDetails({ ...newUserDetails, website: e.target.value })
+            }
             className="max-w-[100%] border border-slate-400 px-2 py-1 rounded-md"
           />
         </label>
 
         <div onClick={handleClick} className="flex flex-wrap gap-3 mt-6">
-          <button className="bg-blue-400 text-white px-4 py-1 rounded-md hover:bg-blue-500">
+          <button
+            disabled={imageUploadLoading}
+            className="bg-blue-400 text-white px-4 py-1 rounded-md hover:bg-blue-500"
+          >
             Save
           </button>
           <button className="bg-gray-400 text-white  px-4 py-1 rounded-md hover:bg-gray-500">
